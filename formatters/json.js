f00d616a3f775cc0a4d361fa605f7e77942c4ba2
Anna Naryzhnaia <999222111@inbox.ru>
2025-06-16 00:09:33 +0300

    fix sorting format for plain and json

--- formatters/json.js
+++ formatters/json.js
@@ -3,7 +3,8 @@ import _ from 'lodash'
 export default (object1, object2) => {
   const propObject1 = Object.keys(object1)
   const propObject2 = Object.keys(object2)
-  const properties = _.union(propObject1, propObject2)
+  const unsortedProperties = _.union(propObject1, propObject2)
+  const properties = _.sortBy(unsortedProperties)
   let difference = {}
   for (const property of properties) {
     if (object1.hasOwnProperty(property) && object2.hasOwnProperty(property)) {
