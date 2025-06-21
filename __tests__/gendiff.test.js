6a2fadd77854790a8b6349407bf718207e9adf38
Anna Naryzhnaia <999222111@inbox.ru>
2025-06-21 23:58:11 +0300

    fix test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -15,7 +15,7 @@ beforeAll(() => {
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
   const getFixturePath = (filename) => {
-    path.join(__dirname, '..', '__fixtures__', filename)
+    return path.join(__dirname, '..', '__fixtures__', filename)
   }
   filePath1 = getFixturePath('json1.json')
   filePath2 = getFixturePath('json2.json')
