5e244b476ca80ae6e65be903e4b65a661438b119
Anna Naryzhnaia <999222111@inbox.ru>
2025-06-23 23:49:43 +0300

    some changes

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,14 @@
-import getStandartDiff from './formatters/standart.js'
-import getPlainDiff from './formatters/plain.js'
-import getJsonDiff from './formatters/json.js'
-import parse from './parsers.js'
+import getStylishDiff from './src/formatters/standart.js'
+import getPlainDiff from './src/formatters/plain.js'
+import getJsonDiff from './src/formatters/json.js'
+import parse from './src/parsers.js'
 
 export default (filePath1, filePath2, format = 'standart') => {
   try {
     const object1 = parse(filePath1)
     const object2 = parse(filePath2)
     if (format === 'standart') {
-      return getStandartDiff(object1, object2)
+      return getStylishDiff(object1, object2)
     }
     else if (format === 'plain') {
       return getPlainDiff(object1, object2)
@@ -17,7 +17,7 @@ export default (filePath1, filePath2, format = 'standart') => {
       return getJsonDiff(object1, object2)
     }
     else {
-      return getStandartDiff(object1, object2)
+      return getStylishDiff(object1, object2)
     }
   }
   catch (e) {
--- src/gendiff.js
+++ src/gendiff.js
@@ -2,7 +2,7 @@
 
 import { Command } from 'commander'
 const program = new Command()
-import genDiff from './index.js'
+import genDiff from '../bin/index.js'
 
 program
   .name('gendiff')
