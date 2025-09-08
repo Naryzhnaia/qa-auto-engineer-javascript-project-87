c38e53e4a01c40a5800db4808f15ed976aca24b2
Anna Naryzhnaia <999222111@inbox.ru>
2025-09-08 20:17:48 +0300

    change json result

--- src/formatters/format.js
+++ src/formatters/format.js
@@ -1,6 +1,5 @@
 import formatStylish from './stylish.js'
 import formatPlain from './plain.js'
-import formatJson from './json.js'
 
 const format = (tree, outputFormat) => {
   switch (outputFormat) {
@@ -9,7 +8,7 @@ const format = (tree, outputFormat) => {
     case 'plain':
       return formatPlain(tree)
     case 'json':
-      return formatJson(tree)
+      return JSON.stringify(tree, null, 0)
     default:
       throw new Error(`Unknown type of format: '${outputFormat}'`)
   }
